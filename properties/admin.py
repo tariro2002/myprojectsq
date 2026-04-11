d4e6bbc770e66fe0e6ade3c05a3936e81aeca13c
Tariro Grace <tarirogracechiadzwa19@gmail.com>
2026-04-11 14:27:07 +0200

    Upgraded API serializer and admin panel

--- properties/admin.py
+++ properties/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
 from .models import Property
 
-admin.site.register(Property)
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    # Columns shown in the list view
+    list_display = ['address', 'owner_name', 'city', 'property_type', 'status', 'value', 'date_registered']
+    
+    # Filters on the right side
+    list_filter = ['property_type', 'status', 'city', 'province']
+    
+    # Search fields
+    search_fields = ['address', 'owner_name', 'owner_email', 'city']
+    
+    # Make status editable directly from list
+    list_editable = ['status']
+    
+    # Order by newest first
+    ordering = ['-date_registered']
+    
+    # Group fields into sections
+    fieldsets = (
+        ('Owner Information', {
+            'fields': ('owner_name', 'owner_email', 'owner_phone')
+        }),
+        ('Property Details', {
+            'fields': ('address', 'city', 'province', 'property_type', 'status', 'size', 'value')
+        }),
+        ('GPS Location', {
+            'fields': ('gps_latitude', 'gps_longitude'),
+            'classes': ('collapse',)
+        }),
+        ('Additional', {
+            'fields': ('image', 'notes', 'registered_by')
+        }),
+    )
--- properties/serializers.py
+++ properties/serializers.py
@@ -1,18 +1,22 @@
 from rest_framework import serializers
 from .models import Property
 
-# This converts Property objects to JSON and back
-# JSON is the language APIs use to send data
-# Example output:
-# {
-#     "id": 1,
-#     "address": "123 Harare Street",
-#     "owner_name": "John Moyo",
-#     "size": "250.00",
-#     "property_type": "house",
-#     "date_registered": "2024-01-01"
-# }
 class PropertySerializer(serializers.ModelSerializer):
+    # This shows the username instead of just the ID
+    registered_by = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Property
-        fields = '__all__'  # Include all fields
+        fields = '__all__'  # Include all fields including new ones
+
+    def validate_size(self, value):
+        # Make sure size is positive
+        if value <= 0:
+            raise serializers.ValidationError("Size must be greater than 0")
+        return value
+
+    def validate_value(self, value):
+        # Make sure value is positive if provided
+        if value and value <= 0:
+            raise serializers.ValidationError("Value must be greater than 0")
+        return value
