f50c92ff884cfd4a5a1f183752f6ba51329e3d47
Tariro Grace <tarirogracechiadzwa19@gmail.com>
2026-04-11 14:38:45 +0200

    Added JWT authentication for API

--- myprojectsq/myprojectsq/urls.py
+++ myprojectsq/myprojectsq/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('properties/', include('properties.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    # JWT Token endpoints for API
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
