28bbf650d5efb44157fb0bfb220e68e2796bd29b
Tariro Grace <tarirogracechiadzwa19@gmail.com>
2026-04-11 15:00:05 +0200

    Fixed JWT token URLs

--- myprojectsq/myprojectsq/urls.py
+++ myprojectsq/myprojectsq/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('properties/', include('properties.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
-    # JWT Token endpoints for API
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
