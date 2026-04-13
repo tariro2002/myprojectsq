1516ebed83384b350eb53a8fc46931168ff33008
Tariro Grace <tarirogracechiadzwa19@gmail.com>
2026-04-13 11:14:40 +0200

    Add purple landing page

--- myprojectsq/urls.py
+++ myprojectsq/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
     path("admin/", admin.site.urls),
     path("properties/", include("properties.urls")),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
