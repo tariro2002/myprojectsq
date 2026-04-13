914d8c631a176e3f081083d06fc98260da641f80
Tariro Grace <tarirogracechiadzwa19@gmail.com>
2026-04-13 09:44:39 +0200

    Fix static files with whitenoise

--- myprojectsq/settings.py
+++ myprojectsq/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,4 +76,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/properties/'
+LOGIN_REDIRECT_URL = '/properties/'STATIC_ROOT = BASE_DIR / 'staticfiles'
